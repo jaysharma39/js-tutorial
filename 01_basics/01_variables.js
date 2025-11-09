0d6ed0455d0c8dc93ff643fd19d4b285d229a881
jaysharma39 <jay.sharma393900@gmail.com>
2025-11-09 17:57:54 +0000

    Container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=2345
+let accountEmail="jay.sharma@gmail.com"
+var accountPassword="12345"
+accountCity="jaipur"
+let accountState;
+
+//accountId=2345
+console.table([accountId,accountEmail,accountPassword,,accountCity,accountState])
+
+/*
+Prefer to use 
+*/
