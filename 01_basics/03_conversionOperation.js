eaa9dc7bca11e66b0b042a4ffaeaf1ed94a9ab24
jaysharma39 <jay.sharma393900@gmail.com>
2025-11-10 11:16:30 +0000

    Conversion of DataTypes done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score="33abc";
+let scoreInNumber=Number(score);
+console.log(typeof scoreInNumber);
+console.log(scoreInNumber);
+
+//score="33"--->33
+//score="33abc"--->NaN(Not a Number)
+//score=true--->1
+//score=false--->0
+//score=null--->0
+
+let isLoggedIn="jayesh";
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+//isLoggedIn=1--->true
+//isLoggedIn=0--->false
+//isLoggedIn="jayesh"--->true
+//isLoggedIn=""--->false
+
+let someNumber=33
+let stringSomeNumber=String(someNumber);
+console.log(typeof stringSomeNumber);
+console.log(stringSomeNumber);
+
+//someNumber=33--->"33"
+//someNumber=true/false/null/undefined--->true/false/null/undefined
+
+
+
+
+
